789cde44441f6815c3e50897002c5b3f7f6acd94
stephanstarikov-hub <stephanstarikov-hub@users.noreply.github.com>
2026-09-07 10:33:55 +0300

    chore: remove accidental shell output files

--- tallerentry.py
+++
@@ -1,8 +0,0 @@
-warning: in the working copy of 'main.py', CRLF will be replaced by LF the next time Git touches it
-warning: in the working copy of 'package.json', LF will be replaced by CRLF the next time Git touches it
-warning: in the working copy of 'vpn/service.py', CRLF will be replaced by LF the next time Git touches it
- main.py        |  41 [32m++++++[m[31m----------[m
- package.json   |   2 [32m+[m[31m-[m
- vpn/service.py | 148 [32m+++++++++++++++++++++++++++++++++++++++++++++++++[m[31m--------[m
- vpn/storage.py |  16 [32m++++++[m[31m-[m
- 4 files changed, 159 insertions(+), 48 deletions(-)
